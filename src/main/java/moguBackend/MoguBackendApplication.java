432bebe6ce15635ba054aef1304b5b5d3d1e61c5
githyuniiee <https://github.com/githyuniiee>
2023-12-03 22:17:43 +0900

    first commit

---
+++ src/main/java/moguBackend/MoguBackendApplication.java
@@ -0,0 +1,13 @@
+package moguBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoguBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoguBackendApplication.class, args);
+	}
+
+}
