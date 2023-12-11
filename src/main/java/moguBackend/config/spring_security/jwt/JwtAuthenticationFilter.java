978d761716d8893eb5bf91186ba45254e8f1f79d
githyuniiee <https://github.com/githyuniiee>
2023-12-11 21:25:42 +0900

    로그인 시 body에 id와 usrname 추가

--- src/main/java/moguBackend/config/spring_security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/moguBackend/config/spring_security/jwt/JwtAuthenticationFilter.java
@@ -81,7 +81,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         response.addHeader(JwtProperties.headerString, jwtToken);
 
-        // 바디에도 토큰 정보 추가 (예시로 username과 id를 추가)
+        // 바디에도 토큰 정보 추가
         Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("username", principalDetailis.getPerson().getUsername());
         responseBody.put("id", principalDetailis.getPerson().getId());
